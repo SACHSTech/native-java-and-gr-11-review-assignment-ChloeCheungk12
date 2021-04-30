e0c913cb1e19764f9022c5751f91fecd2b92ccee
ChloeCheungk12 <chloe.cheung21@ycdsbk12.ca>
2021-04-30 13:35:58 -0400

    Review 2

--- src/gr11review/part1/Review1.java
+++ src/gr11review/part1/Review1.java
@@ -1,1 +1,36 @@
 package gr11review.part1;
+import java.io.*;
+
+public class Review1{
+  public static void main(String[] args) throws IOException{
+    BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+
+    // Declare variables
+    int intMonth;
+    int intDay;
+    int intNumDays;
+
+    // Initialize Variable
+    intNumDays = 0;
+    
+    // Get inputs
+    System.out.print("Enter the month number: ");
+    intMonth = Integer.parseInt(key.readLine());
+    System.out.print("Enter the day number: ");
+    intDay = Integer.parseInt(key.readLine());
+
+    for (int intCount = 1; intCount < intMonth; intCount++) {
+      if (intCount == 2) {
+        intNumDays += 28;
+      } 
+      else if (intCount == 4 || intCount == 6 || intCount == 9 || intCount == 11) {
+        intNumDays += 30;
+      } 
+      else {
+        intNumDays += 31;
+      }
+    }
+
+    System.out.println(intNumDays + intDay);
+  }
+}
--- src/gr11review/part1/Review3.java
+++ src/gr11review/part1/Review3.java
@@ -1,1 +1,17 @@
 package gr11review.part1;
+
+import java.io.*;
+
+public class Review3{
+  public static void main(String[] args) throws IOException{
+        
+        for (int intCount = 21; intCount < 100; intCount++) {
+            System.out.println(intCount);
+            intCount += 1;
+        }
+        System.out.println("");
+        for (int intCount = 29; intCount >=2; intCount--) {
+            System.out.println(intCount);
+        }
+    }
+}
